56ef405644f97cd3ba4ace4368bffe1b2c88824b
Thariq <thariq879@gmail.com>
2023-07-30 09:07:57 +0700

    update todo module and try create table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodoModule } from './todos/todos.module';
+import { Todo } from './todos/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'testing_crud',
+        entities: [Todo],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
